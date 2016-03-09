c2ea14ff7b29022d18870763622e6712072959fe
Daniel Fineberg <danielfineberg@hotmail.co.uk>
2016-03-09 10:04:27 +0000

    Added scripts folder to git repo

---
+++ Assets/Scripts/Move.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+    float speed, targetLifetime, lifetime;
+    Vector3 direction;
+
+    void Update()
+    {
+        transform.Translate(direction * speed * Time.deltaTime);
+        lifetime += Time.deltaTime;
+
+        if(lifetime >= targetLifetime)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Init(Vector3 direction, float speed, float lifetime)
+    {
+        targetLifetime = lifetime;
+        this.speed = speed;
+        this.direction = direction.normalized;
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class PointInfo
+{
+    public Transform point;
+    public bool reversed;
+}
+
+[System.Serializable]
+public class Tileset
+{
+    public PointInfo[] points;
+    public GameObject[] tilePrefabs;
+    public float scrollSpeed = 1f, lifetime = 10f, frequency = 10f;
+    public float rotation, reversedRotation;
+}
+
+public class Spawner : MonoBehaviour {
+
+    public Tileset[] tilesets;
+
+
+    void Start()
+    {
+        foreach(Tileset tileset in tilesets)
+        {
+            StartCoroutine(SpawnRoutine(tileset));
+        }
+    }
+
+    IEnumerator SpawnRoutine(Tileset tileset)
+    {
+        while (true)
+        {
+            foreach (PointInfo point in tileset.points)
+            {
+                GameObject randomPrefab = tileset.tilePrefabs[Random.Range(0, tileset.tilePrefabs.Length)];
+                GameObject newTile = (GameObject)Instantiate(randomPrefab, point.point.position, Quaternion.Euler(0f, point.reversed ? tileset.reversedRotation : tileset.rotation, 0f));
+                Vector3 moveDirection = point.point.TransformDirection(Vector3.forward);
+                Move move = newTile.AddComponent<Move>();
+                move.Init(moveDirection, tileset.scrollSpeed, tileset.lifetime);
+            }
+
+            yield return new WaitForSeconds(tileset.frequency);
+        }
+    }
+}
